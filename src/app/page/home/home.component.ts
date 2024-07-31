7e29a6cc24f611f035cd9a84c0f0913ce7833e79
Vijin <vijinrobinson14@gmail.com>
2024-07-31 16:54:34 +0530

    feat:graph_v1

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GraphComponent } from '../../ui/graph/graph.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [GraphComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/ui/graph/graph.component.ts
+++ src/app/ui/graph/graph.component.ts
@@ -1,153 +1,109 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-graph',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './graph.component.html',
-//   styleUrl: './graph.component.css'
-// })
-// export class GraphComponent {
-
-// }
-
-/* app.component.ts */
 import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
- 
-import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
-
 import { ChartModule } from 'primeng/chart';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule, } from 'primeng/calendar';
+
  
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CanvasJSAngularChartsModule, ChartModule],
+  imports: [CommonModule, RouterOutlet, ChartModule, FormsModule, CalendarModule, ],
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent  {
-	title = 'angular17ssrapp';
-	generateRandomData = () => {
-		var y  = 24, dps = [];
-		for(var i = 0; i < 24; i++) {
-			y += Math.ceil(Math.random() * 10 - 5);
-			dps.push({ y: y});
-		}
-    console.log(dps);
-    
-		return dps;
-	}
-
-  chartData: {y: number, x: number}[] = [
-    {y:33,x:1},
-    {y:35, x:2},
-    {y:37, x:3},
-    {y:36, x:4},
-    {y:37, x:5},
-    {y:33, x:6},
-    {y:34, x:7},
-    {y:36, x:8},
-    {y:34, x:9},
-    {y:33, x:10},
-    {y:37, x:11},
-    {y:37, x:12},
-    {y:36, x:13},
-    {y:35, x:14},
-    {y:35, x:15},
-    {y:36, x:16},
-    {y:36, x:17},
-    {y:36, x:18},
-    {y:36, x:19},
-    {y:37, x:20},
-    {y:35, x:21},
-    {y:33, x:22},
-    {y:34, x:23},
-    {y:34, x:24},
-    {y:36, x:25},
-    {y:36, x:26}
-  ];
-
-	chartOptions = {
-	  zoomEnabled: true,
-	  exportEnabled: true,
-	  theme: "light2",
-	  title: {
-		text: "Batch 4 Attendance Chart"
-	  },
-	  data: [{
-		type: "line",
-    color: "#EA454C",
-		dataPoints: this.chartData
-	  }]
-	}
-
-
-
-  myLable = new Date();
-  // console.log(myLable);
-  
+  graphDataMonth: Date | undefined;
+  numberOfWorkingDays: number = 25;
   data: any;
+  options: any;
+  monthNames = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+  currentMonth:string = '';
+  maxDate!: Date;
 
-    options: any;
+  ngOnInit() {
+    this.graphDataMonth = new Date();
+    this.maxDate = this.graphDataMonth;
+    this.currentMonth = this.monthNames[this.graphDataMonth.getMonth()]
+    this.graphInit();    
+  }
 
-    ngOnInit() {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('--text-color');
-        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+  generateRandomData(length: number): number[] {
+    const min = 34;
+    const max = 37;
+    const result: number[] = [];
+    for (let i = 0; i < length; i++) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      result.push(randomNumber);
+    }
+    return result;
+  }
+  generateXaxisLabel(length: number): string[] {
+    const result: string[] = []; 
+    for (let i = 1; i <= length; i++) {
+      result.push(i+this.currentMonth);
+    }
+    return result;
+  } // for testing with data
 
-        this.data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'],
-            datasets: [
-                {
-                    label: 'Present',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    borderColor: documentStyle.getPropertyValue('--blue-500'),
-                    tension: 0.4
-                },
-                // {
-                //     label: 'Second Dataset',
-                //     data: [28, 48, 40, 19, 86, 27, 90],
-                //     fill: false,
-                //     borderColor: documentStyle.getPropertyValue('--pink-500'),
-                //     tension: 0.4
-                // }
-            ]
-        };
+  monthSelection() {
+    this.currentMonth = this.monthNames[<number>this.graphDataMonth?.getMonth()]
+    this.graphInit()
+  }
 
-        this.options = {
-            maintainAspectRatio: false,
-            aspectRatio: 0.6,
-            plugins: {
-                legend: {
-                    labels: {
-                        color: textColor
-                    }
-                }
+  graphInit(){
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.data = {
+        labels: this.generateXaxisLabel(this.numberOfWorkingDays),
+        datasets: [
+            {
+                label: 'Present',
+                data: this.generateRandomData(this.numberOfWorkingDays),
+                fill: false,
+                borderColor: "#EA454C",
+                tension: 0.4
             },
-            scales: {
-                x: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
+        ]
+    };
+    //remove aspect ration to alter height, or change it to alter height
+    this.options = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.9,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
                 }
             }
-        };
-    }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            }
+        }
+    };
+  }
 }        
