08f464397c08e23ae9183ffdb5da66b2e0a72ca2
FAHEEM133 <mhd.faheem00@gmail.com>
2024-08-01 01:26:52 +0530

    insert widget cards to dashboard

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TopHeaderComponent } from '../../ui/top-header/top-header.component';
 import { CurrentDateComponent } from '../../ui/current-date/current-date.component';
+import { WidgetCardsComponent } from "../../ui/widget-cards/widget-cards.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TopHeaderComponent,CurrentDateComponent],
+  imports: [TopHeaderComponent, CurrentDateComponent, WidgetCardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
