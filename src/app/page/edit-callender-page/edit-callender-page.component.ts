187f273f09eef904eaa8d90788c928cfbdb8590f
pgsaihari <saiharinarayan@gmail.com>
2024-07-31 10:02:29 +0530

    file structure update

---
+++ src/app/page/edit-callender-page/edit-callender-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-callender-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-callender-page.component.html',
+  styleUrl: './edit-callender-page.component.css'
+})
+export class EditCallenderPageComponent {
+
+}
