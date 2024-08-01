74c4c9344b7b173be7788108b8b8b65e3ccbd084
irshadhussain21 <irshadhussain120@gmail.com>
2024-08-01 11:45:49 +0530

    localcommit

--- src/app/ui/widget-card/widget-card.component.ts
+++ src/app/ui/widget-card/widget-card.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
+import { Component,Input } from '@angular/core';
+import { CardModule } from "primeng/card";
 @Component({
   selector: 'app-widget-card',
   standalone: true,
-  imports: [CardModule,ButtonModule],
+  imports: [CardModule],
   templateUrl: './widget-card.component.html',
   styleUrl: './widget-card.component.css'
 })
 export class WidgetCardComponent {
-  card_number: Number = 456;
-  header_icon: string = 'fa-solid fa-users employee-icon';
-  card_header: string = 'Total Trainees';
+  @Input() header_icon!: string ;
+@Input() card_number!: Number;
+@Input() card_header!: string;
 }
