6292b26de77dabc93a5a294051b23b0a6961a218
pgsaihari <saiharinarayan@gmail.com>
2024-08-01 09:26:38 +0530

    feat:homeV1

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TopHeaderComponent } from '../../ui/top-header/top-header.component';
 import { CurrentDateComponent } from '../../ui/current-date/current-date.component';
+import { GraphComponent } from "../../ui/graph/graph.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TopHeaderComponent,CurrentDateComponent],
+  imports: [TopHeaderComponent, CurrentDateComponent, GraphComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
