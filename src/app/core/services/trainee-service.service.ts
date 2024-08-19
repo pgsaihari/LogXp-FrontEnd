262a4b5801fb57f2bbec381cca93f38c7e25d058
pgsaihari <saiharinarayan@gmail.com>
2024-08-19 18:20:52 +0530

    "updated services"

--- src/app/core/services/trainee-service.service.ts
+++ src/app/core/services/trainee-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,25 +13,28 @@ export class TraineeServiceService {
 
   // Function to get all trainees
   getTrainees(): Observable<any[]> {
-    return this.http.get<{ trainees: any[] }>(this.apiUrl).pipe(
-      map(response => response.trainees)
-    );
+    return this.http.get<any[]>(this.apiUrl);
   }
 
   // Function to get count of active trainees
   getTraineesCount(): Observable<number> {
-    return this.http.get<number>(this.apiUrl + '/active-count');
+    return this.http.get<number>(`${this.apiUrl}/active-count`);
   }
 
-  // Function to get a trainee by ID
-  getTraineeById(id: number): Observable<any> {
-    const url = `${this.apiUrl}/${id}`;
+  // Function to get a trainee by employeeCode
+  getTraineeByEmployeeCode(employeeCode: string): Observable<any> {
+    const url = `${this.apiUrl}/${employeeCode}`;
     return this.http.get<any>(url);
   }
 
   // Function to add a new trainee
   addTrainee(trainee: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, trainee);
+    return this.http.post<any>(`${this.apiUrl}/add-trainee`, trainee);
+  }
+
+  // Function to add multiple trainees
+  addTrainees(trainees: any[]): Observable<any> {
+    return this.http.post<any>(this.apiUrl, trainees);
   }
 
   // Function to update an existing trainee
@@ -40,9 +43,15 @@ export class TraineeServiceService {
     return this.http.put<any>(url, trainee);
   }
 
+  // Function to update trainee status
+  updateTraineeStatus(employeeCode: string, isActive: boolean): Observable<any> {
+    const url = `${this.apiUrl}/${employeeCode}/status`;
+    return this.http.patch<any>(url, { isActive });
+  }
+
   // Function to delete a trainee
-  deleteTrainee(id: number): Observable<any> {
-    const url = `${this.apiUrl}/${id}`;
+  deleteTrainee(employeeCode: string): Observable<any> {
+    const url = `${this.apiUrl}/${employeeCode}`;
     return this.http.delete<any>(url);
   }
 }
