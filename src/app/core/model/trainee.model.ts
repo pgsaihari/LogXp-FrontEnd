17d089b2b606d08ad6c1883c92a7292266addb06
SamvruthaShaji <samvrutha.shaji01@gmail.com>
2024-08-22 11:48:15 +0530

    updated trainee table

--- src/app/core/model/trainee.model.ts
+++ src/app/core/model/trainee.model.ts
@@ -2,7 +2,7 @@ export interface Trainee {
     employeeCode? : string;        // EmployeeCode is the primary key
     name?: string;               // Optional property
     email?: string;              // Optional property
-    batch?: string;              // Optional property
+    batchId?: number;              // Optional property
     isActive?:boolean;
   }
   
