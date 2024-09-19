63ed09dbd51c5d4f65c232b0a786b7c5883f1cd7
pgsaihari <saiharinarayan@gmail.com>
2024-09-19 11:00:57 +0530

    "new"

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'api/logXP' // Local development URL
+    apiUrl: 'http://172.16.4.89:9021/api/logXP' // Local development URL
   };
   
