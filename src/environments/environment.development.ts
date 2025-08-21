65221d23b87ca1dbd2591500bb423dff43a85bfc
Vijin R <vijin.r@experionglobal.com>
2025-08-21 18:46:14 +0530

    changed to new domain url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://13.200.48.60/api/logXP' // Local development URL
+    apiUrl: 'https://logxp.experionglobal.com/api/logXP' // Local development URL
   };
